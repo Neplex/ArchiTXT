1bc4f31d7eb047ba5a31e268ab0feecd0af50ca8
Nicolas H. <nicolas.hiot@univ-orleans.fr>
2024-10-09 14:53:39 +0200

    Refactor instance generator and add doctest

--- architxt/generator/__init__.py
+++ architxt/generator/__init__.py
@@ -2,26 +2,33 @@
 Generator of instances
 """
 
+from collections.abc import Generator, Iterable
+
 from architxt.model import NodeLabel, NodeType
-from architxt.tree import ParentedTree, Tree
+from architxt.tree import ParentedTree
 
 GROUP_SCHEMA = dict[str, tuple[str, ...]]
 REL_SCHEMA = dict[str, tuple[str, str]]
 
 
-def gen_group(name: str, elements: tuple[str, ...]) -> Tree:
+def gen_group(name: str, elements: tuple[str, ...]) -> ParentedTree:
     """
     Generates a group tree structure with the given name and elements.
     :param name: The name of the group.
     :param elements: The elements to include in the group.
     :return: The generated group tree.
+
+    Example:
+    >>> group_tree = gen_group('Fruits', ('Apple', 'Banana', 'Cherry'))
+    >>> print(group_tree.pformat(margin=255))
+    (GROUP::Fruits (ENT::Apple ) (ENT::Banana ) (ENT::Cherry ))
     """
     label = NodeLabel(NodeType.GROUP, name)
-    element_trees = [Tree(NodeLabel(NodeType.ENT, element), []) for element in elements]
-    return Tree(label, element_trees)
+    element_trees = [ParentedTree(NodeLabel(NodeType.ENT, element), []) for element in elements]
+    return ParentedTree(label, element_trees)
 
 
-def gen_relation(name: str, sub: str, obj: str, groups: GROUP_SCHEMA) -> Tree:
+def gen_relation(name: str, sub: str, obj: str, groups: GROUP_SCHEMA) -> ParentedTree:
     """
     Generates a relation tree structure based on the given parameters.
     :param name: The name of the relationship.
@@ -29,27 +36,40 @@ def gen_relation(name: str, sub: str, obj: str, groups: GROUP_SCHEMA) -> Tree:
     :param obj: The object of the relationship.
     :param groups: A dictionary containing group schemas.
     :return: The generated relation tree.
+
+    Example:
+    >>> groups = {'Fruits': ('Apple', 'Banana'), 'Colors': ('Red', 'Blue')}
+    >>> relation_tree = gen_relation('Preference', 'Fruits', 'Colors', groups)
+    >>> print(relation_tree.pformat(margin=255))
+    (REL::Preference (GROUP::Fruits (ENT::Apple ) (ENT::Banana )) (GROUP::Colors (ENT::Red ) (ENT::Blue )))
     """
     label = NodeLabel(NodeType.REL, name)
     subject_tree = gen_group(sub, groups[sub])
     object_tree = gen_group(obj, groups[obj])
-    return Tree(label, [subject_tree, object_tree])
+    return ParentedTree(label, [subject_tree, object_tree])
 
 
-def gen_collection(name: str, elements: list[Tree]) -> Tree:
+def gen_collection(name: str, elements: Iterable[ParentedTree]) -> ParentedTree:
     """
     Generate a collection tree.
     :param name: The name of the collection.
     :param elements: The list of trees that make up the collection.
     :return: A tree representing the collection.
+
+    Example:
+    >>> from architxt.tree import Tree
+    >>> elems = [ParentedTree('Element1', []), ParentedTree('Element2', [])]
+    >>> collection_tree = gen_collection('Collection', elems)
+    >>> print(collection_tree.pformat(margin=255))
+    (COLL::Collection (Element1 ) (Element2 ))
     """
     label = NodeLabel(NodeType.COLL, name)
-    return Tree(label, elements)
+    return ParentedTree(label, elements)
 
 
 def gen_instance(
-    groups: GROUP_SCHEMA, rels: REL_SCHEMA, size: int = 200, generate_collections: bool = True
-) -> ParentedTree:
+    groups: GROUP_SCHEMA, rels: REL_SCHEMA, *, size: int = 200, generate_collections: bool = True
+) -> Generator[ParentedTree, None, None]:
     """
     Generate a database instances as a tree based on the given groups and relations schema.
     :param groups: A dictionary containing group names as keys and elements as values.
@@ -58,29 +78,22 @@ def gen_instance(
     :param generate_collections: A boolean indicating whether to generate collections or not.
     :return: A ParentedTree instance representing the generated instance.
     """
-    generated_trees = []
-
-    # Groups generation
+    # Generate tree instances for each group
     for group_name, elements in groups.items():
-        generated = [gen_group(group_name, elements) for _ in range(size)]
+        generated = (gen_group(group_name, elements) for _ in range(size))
 
         if generate_collections:
-            coll = gen_collection(group_name, generated)
-            generated_trees.append(coll)
+            yield gen_collection(group_name, generated)
 
         else:
-            generated_trees.extend(generated)
+            yield from generated
 
-    # Relations generation
+    # Generate tree instances for each relation
     for rel_name, (sub, obj) in rels.items():
         generated = (gen_relation(rel_name, sub, obj, groups) for _ in range(size))
 
         if generate_collections:
-            coll = gen_collection(rel_name, generated)
-            generated_trees.append(coll)
+            yield gen_collection(rel_name, generated)
 
         else:
-            generated_trees.extend(generated)
-
-    root = Tree('ROOT', generated_trees)
-    return ParentedTree.convert(root)
+            yield from generated
