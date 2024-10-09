4680890d637c7425cac7ee3ecece12f36de8fa9e
Nicolas H. <nicolas.hiot@univ-orleans.fr>
2024-10-09 14:35:57 +0200

    Use cache to avoid reparsing the corpus every time

--- architxt/cli/__init__.py
+++ architxt/cli/__init__.py
@@ -4,6 +4,7 @@ from pathlib import Path
 import mlflow
 import typer
 from nltk import Production
+from ray import cloudpickle
 
 from architxt.algo import rewrite
 from architxt.generator import gen_instance
@@ -20,6 +21,7 @@ def cli(
     min_support: int = 10,
     corenlp_url: str = 'http://localhost:9001',
     gen_instances: int = 0,
+    language: str = 'French',
 ):
     mlflow.log_params(
         {
@@ -28,15 +30,28 @@ def cli(
         }
     )
 
-    print(f'Load corpus: {corpus_path.absolute()}')
-    sentences = get_sentence_from_disk(
-        corpus_path,
-        entities_filter={'MOMENT', 'DUREE', 'DATE'},
-        relations_filter={'TEMPORALITE', 'CAUSE-CONSEQUENCE'},
-        entities_mapping={'FREQ': 'FREQUENCE'},
-    )
-    forest = ParentedTree('ROOT', get_enriched_forest(sentences, corenlp_url=corenlp_url))
-    print('Dataset loaded!')
+    corpus_cache_path = Path() / f'{corpus_path.name}.pkl'
+    if corpus_cache_path.exists():
+        print(f'Load corpus from cache: {corpus_cache_path.absolute()}')
+        with open(corpus_cache_path, 'rb') as cache_file:
+            trees = cloudpickle.load(cache_file)
+
+    else:
+        print(f'Load corpus from disk: {corpus_path.absolute()}')
+        sentences = get_sentence_from_disk(
+            corpus_path,
+            entities_filter={'MOMENT', 'DUREE', 'DATE'},
+            relations_filter={'TEMPORALITE', 'CAUSE-CONSEQUENCE'},
+            entities_mapping={'FREQ': 'FREQUENCE'},
+        )
+        trees = tuple(get_enriched_forest(sentences, corenlp_url=corenlp_url, language=language))
+
+        print(f'Save cache file to: {corpus_cache_path.absolute()}')
+        with open(corpus_cache_path, 'wb') as cache_file:
+            cloudpickle.dump(trees, cache_file)
+
+    # forest = ParentedTree('ROOT', trees)
+    print(f'Dataset loaded! {len(trees)} sentences found')
 
     if gen_instances:
         print('Generate instance...')
@@ -52,10 +67,11 @@ def cli(
             },
             size=gen_instances,
         )
-        forest = forest.merge(gen_trees)
+        trees.extend(gen_trees)
 
     # mlflow.log_param('nb_sentences', len(trees))
     with open('debug.txt', 'w', encoding='utf8') as log_file:
+        forest = ParentedTree('ROOT', trees)
         final_tree = rewrite(forest, tau=tau, epoch=epoch, min_support=min_support, stream=log_file)
     print('Done!')
 
