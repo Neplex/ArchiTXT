3def56788c2658a2048d78970eb0926c25cd266a
Nicolas H. <nicolas.hiot@univ-orleans.fr>
2024-12-03 11:23:46 +0100

    Refactor rewriting step

--- architxt/algo.py
+++ architxt/algo.py
@@ -5,12 +5,14 @@ from copy import deepcopy
 from multiprocessing import cpu_count
 
 import mlflow
-from nltk import Production, TreePrettyPrinter
+from nltk import TreePrettyPrinter
 from tqdm import trange
 from tqdm.contrib.concurrent import process_map
 
 from architxt import operations
+from architxt.db import Schema
 from architxt.model import NodeType
+from architxt.operations import OPERATION
 from architxt.similarity import DEFAULT_METRIC, METRIC_FUNC, TREE_CLUSTER, equiv_cluster
 from architxt.tree import Forest, Tree, has_type, reduce_all
 
@@ -36,7 +38,23 @@ def rewrite(
     debug: bool = False,
     max_workers: int | None = None,
 ) -> Forest:
+    """
+    Iteratively rewrites a forest by applying edit operations.
+
+    :param forest: The forest to perform on.
+    :param tau: Threshold for subtree similarity when clustering.
+    :param epoch: Maximum number of rewriting steps.
+    :param min_support: Minimum support of groups.
+    :param metric:  The metric function used to compute similarity between subtrees.
+    :param edit_ops: The list of operations to perform on the forest.
+    :param debug: Whether to enable debug logging.
+    :param max_workers: Number of parallel worker processes to use.
+
+    :return: The rewritten forest.
+    """
     min_support = min_support or max((len(forest) // 10), 2)
+    max_workers = max_workers or cpu_count()
+
     mlflow.log_params(
         {
             'nb_sentences': len(forest),
@@ -59,10 +77,11 @@ def rewrite(
                     metric=metric,
                     edit_ops=edit_ops,
                     debug=debug,
-                    max_workers=max_workers or cpu_count(),
+                    max_workers=max_workers,
                 )
 
-                if iteration != 0 and not has_simplified:
+                # Stop if no further simplifications are made
+                if iteration > 0 and not has_simplified:
                     break
 
     return forest
@@ -78,7 +97,21 @@ def _rewrite_step(
     edit_ops: Sequence[operations.OPERATION],
     debug: bool,
     max_workers: int,
-):
+) -> tuple[Forest, bool]:
+    """
+    Perform a single rewrite step on the forest.
+
+    :param iteration: The current iteration number.
+    :param forest: The forest to perform on.
+    :param tau: Threshold for subtree similarity when clustering.
+    :param min_support: Minimum support of groups.
+    :param metric:  The metric function used to compute similarity between subtrees.
+    :param edit_ops: The list of operations to perform on the forest.
+    :param debug: Whether to enable debug logging.
+    :param max_workers: Number of parallel worker processes to use.
+
+    :return: The updated forest and a flag indicating if simplifications occurred.
+    """
     if debug:
         # Log the forest as SVG
         rooted_forest = Tree('ROOT', deepcopy(forest))
@@ -104,28 +137,23 @@ def _rewrite_step(
 
     has_simplified = False
     for op_id, edit_op in enumerate(edit_ops):
-        operation = functools.partial(
-            edit_op, equiv_subtrees=equiv_subtrees, tau=tau, min_support=min_support, metric=metric
+        forest, simplified = _apply_operation(
+            edit_op,
+            forest,
+            equiv_subtrees=equiv_subtrees,
+            tau=tau,
+            min_support=min_support,
+            metric=metric,
+            max_workers=max_workers,
         )
 
-        with mlflow.start_span(edit_op.__name__):
-            result = process_map(
-                operation,
-                forest,
-                max_workers=max_workers,
-                chunksize=int(len(forest) // max_workers // 6),
-                leave=False,
-                desc=edit_op.__name__,
-            )
-
-        forest, simplified = zip(*result)
         if has_simplified := any(simplified):
             mlflow.log_metric('edit_op', op_id, step=iteration)
             break
 
-    _post_process(forest, tau, metric, max_workers=max_workers)
+    forest = _post_process(forest, tau, metric, max_workers=max_workers)
 
-    # _log_productions(rooted_forest)
+    _log_schema(iteration, forest)
     _log_metrics(iteration, forest, equiv_subtrees)
 
     return forest, has_simplified
@@ -136,72 +164,93 @@ def _post_process(
     tau: float,
     metric: METRIC_FUNC,
     max_workers: int,
-) -> None:
+) -> Forest:
+    """
+    Post-process the forest to find and name relations and collections.
+
+    :param forest: The forest to perform on.
+    :param tau: Threshold for subtree similarity when clustering.
+    :param metric: The metric function used to compute similarity between subtrees.
+    :param max_workers: Number of parallel worker processes to use.
+
+    :returns: The processed forest with named relations and collections.
+    """
     equiv_subtrees = equiv_cluster(forest, tau=tau, metric=metric)
 
-    fn = functools.partial(
+    forest, _ = _apply_operation(
         operations.find_relations,
+        forest,
+        equiv_subtrees=equiv_subtrees,
         tau=tau,
         min_support=0,
         metric=metric,
-        equiv_subtrees=equiv_subtrees,
         naming_only=True,
+        max_workers=max_workers,
+        desc='[post-process] name_relations',
     )
-
-    forest, _ = zip(
-        *process_map(
-            fn,
-            forest,
-            max_workers=max_workers,
-            chunksize=100,
-            leave=False,
-            desc='[post-process] name_relations',
-        )
-    )
-
-    fn = functools.partial(
+    forest, _ = _apply_operation(
         operations.find_collections,
+        forest,
+        equiv_subtrees=equiv_subtrees,
         tau=tau,
         min_support=0,
         metric=metric,
-        equiv_subtrees=equiv_subtrees,
         naming_only=True,
+        max_workers=max_workers,
+        desc='[post-process] name_collections',
     )
 
-    forest, _ = zip(
-        *process_map(
-            fn,
+    return forest
+
+
+def _apply_operation(
+    operation: OPERATION,
+    forest: Forest,
+    *,
+    equiv_subtrees: TREE_CLUSTER,
+    tau: float,
+    min_support: int,
+    metric: METRIC_FUNC,
+    max_workers: int,
+    desc: str = '',
+    **kwargs,
+) -> tuple[Forest, tuple[bool]]:
+    """
+    Apply the given operation to the forest.
+
+    :param operation: The rewriting operation to apply.
+    :param forest: The forest to perform on.
+    :param equiv_subtrees: The set of equivalent subtrees.
+    :param tau: Threshold for subtree similarity when clustering.
+    :param min_support: Minimum support of groups.
+    :param metric: The metric function used to compute similarity between subtrees.
+    :param max_workers: Number of parallel worker processes to use.
+    :param desc: The description for tqdm progress bar, default to operation name.
+    :return: The rewritten forest and the tuple of flags for each tree indicating if simplifications occurred.
+    """
+    desc = desc or operation.__name__
+    chunk_size = max(10, len(forest) // (max_workers * 6))
+
+    operation_fn = functools.partial(
+        operation,
+        equiv_subtrees=equiv_subtrees,
+        tau=tau,
+        min_support=min_support,
+        metric=metric,
+        **kwargs,
+    )
+
+    with mlflow.start_span(desc):
+        result = process_map(
+            operation_fn,
             forest,
             max_workers=max_workers,
-            chunksize=100,
+            chunksize=chunk_size,
             leave=False,
-            desc='[post-process] name_collections',
+            desc=desc,
         )
-    )
 
-
-def _get_productions(forest: Forest) -> Counter[str]:
-    """
-    Extracts and counts non-lexical productions from a forest.
-
-    :param forest: A forest to extract productions from.
-    :return: A `Counter` object containing the counts of each production rule.
-    """
-    production_counter: Counter[str] = Counter()
-
-    for tree in forest:
-        for production in tree.productions():
-            if production.is_lexical():
-                continue  # Skip lexical productions
-
-            lhs = production.lhs().symbol()
-            rhs = sorted(symbol.symbol() for symbol in production.rhs())
-            rhs_str = f'{rhs[0]}+' if has_type(lhs, NodeType.COLL) else ' '.join(rhs)
-
-            production_rule = f'{lhs} -> {rhs_str}'
-            production_counter.update([production_rule])
-
-    return production_counter
+    return zip(*result)
 
 
 def _log_metrics(iteration: int, forest: Forest, equiv_subtrees: TREE_CLUSTER = ()):
@@ -216,9 +265,6 @@ def _log_metrics(iteration: int, forest: Forest, equiv_subtrees: TREE_CLUSTER = 
     :param equiv_subtrees: A set of clusters representing equivalent subtrees.
     :return: None
     """
-    # Compute the number of production rules in the forest
-    num_productions = len(_get_productions(forest))
-
     # Count labels for all nodes in the forest
     label_counts = Counter(subtree.label() for tree in forest for subtree in tree.subtrees())
 
@@ -248,7 +294,6 @@ def _log_metrics(iteration: int, forest: Forest, equiv_subtrees: TREE_CLUSTER = 
     # Log the calculated metrics
     mlflow.log_metrics(
         {
-            'num_productions': num_productions,
             'num_non_terminal_nodes': len(label_counts),
             'num_unlabeled_nodes': num_unlabeled,
             'num_equiv_subtrees': len(equiv_subtrees),
@@ -298,10 +343,13 @@ def _log_clusters(iteration: int, equiv_subtrees: TREE_CLUSTER):
     )
 
 
-def _log_productions(root_tree: Tree):
-    production_count = Counter(root_tree.productions())
-    for production, count in production_count.most_common():
-        production: Production
-        if production.is_nonlexical():
-            # stream.write(f'[{count}] {production}\n')
-            pass
+def _log_schema(iteration: int, forest: Forest):
+    """
+    Log the schema to MLFlow.
+    :param iteration: The current iteration number for logging.
+    :param forest: A forest of tree objects to analyze.
+    """
+    schema = Schema.from_forest(forest)
+
+    mlflow.log_metric('num_productions', len(schema.productions()))
+    mlflow.log_text(schema.as_cfg(), f'debug/{iteration}/schema.txt')
