06e6ef5cc27f3cfdf37d021dd1ec006c67520ff0
Nicolas H. <nicolas.hiot@univ-orleans.fr>
2025-03-05 18:14:09 +0100

    :memo: Add Sphinx documentation

--- docs/conf.py
+++ docs/conf.py
@@ -14,9 +14,9 @@ sys.path.insert(0, str(Path('..').resolve().absolute()))
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
 project = 'ArchiTXT'
-copyright = '2024, Nicolas Hiot, Mirian Halfeld-Ferrari, Jacques Chabin'
+copyright = '2024, Nicolas Hiot, Mirian Halfeld-Ferrari, Jacques Chabin'  # noqa: A001
 author = 'Nicolas Hiot, Mirian Halfeld-Ferrari, Jacques Chabin'
-release = '1.0.0'
+release = '0.1.0'
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
@@ -26,6 +26,7 @@ extensions = [
     'sphinx.ext.autodoc.typehints',
     'sphinx.ext.autosummary',
     'sphinx.ext.coverage',
+    'sphinx_rtd_theme',
 ]
 
 autodoc_typehints = "description"
@@ -37,5 +38,5 @@ exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = 'alabaster'
+html_theme = 'sphinx_rtd_theme'
 html_static_path = ['_static']
